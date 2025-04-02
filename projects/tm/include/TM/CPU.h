d52295f52347194f2223f1ac227b32ddb668289e
Dennis Griffin <dgdev1024@gmail.com>
2025-04-02 12:24:54 -0400

    TM: Working on CPU.

---
+++ projects/tm/include/TM/CPU.h
@@ -0,0 +1,335 @@
+/**
+ * @file  TM/CPU.h
+ * @brief Contains the TM virtual CPU structure and function prototypes.
+ */
+
+#pragma once
+#include <TM/Common.h>
+
+// Typedefs and Forward Declarations ///////////////////////////////////////////////////////////////
+
+/**
+ * @brief   Forward declaration of the TM virtual CPU structure.
+ */
+typedef struct TM_CPU TM_CPU;
+
+/**
+ * @brief   Pointer to a function called by the CPU's functions to read a byte from the bus.
+ * @param   p_Address The 32-bit address to read from.
+ * @return  The byte read from the bus.
+ */
+typedef uint8_t (*TM_BusRead) (uint32_t);
+
+/**
+ * @brief   Pointer to a function called by the CPU's functions to write a byte to the bus.
+ * @param   p_Address The 32-bit address to write to.
+ * @param   p_Data    The byte to write to the bus.
+ */
+typedef void (*TM_BusWrite) (uint32_t, uint8_t);
+
+/**
+ * @brief   Pointer to a function called by the CPU whenever a CPU cycle is completed. This function
+ *          is to tick the other hardware components attached to the CPU's bus.
+ * @param   p_Cycles  The number of cycles to tick the other components.
+ * @return  `true` if all of the components were ticked successfully; `false` otherwise.
+ */
+typedef bool (*TM_Cycle) (uint32_t);
+
+// Public Function Prototypes //////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief   Creates a new instance of the TM virtual CPU.
+ * @param   p_BusRead  The function to read from the bus.
+ * @param   p_BusWrite The function to write to the bus.
+ * @param   p_Cycle    The function to call when a CPU cycle is completed.
+ * @return  A pointer to the new TM CPU instance.
+ */
+TM_CPU* TM_CreateCPU (TM_BusRead p_BusRead, TM_BusWrite p_BusWrite, TM_Cycle p_Cycle);
+
+/**
+ * @brief   Resets the TM CPU instance, setting its registers and state to their initial values.
+ * @param   p_CPU     The TM CPU instance to reset.
+ */
+void TM_ResetCPU (TM_CPU* p_CPU);
+
+/**
+ * @brief   Destroys the given TM CPU instance, freeing its memory.
+ * @param   p_CPU     The TM CPU instance to destroy.
+ */
+void TM_DestroyCPU (TM_CPU* p_CPU);
+
+/**
+ * @brief   Cycles the CPU for a specified number of cycles, ticking the other components attached
+ *          to the CPU's bus.
+ * 
+ * Every time the CPU accesses the address bus (to read a byte from, or write a byte to) or modifies
+ * its program counter or stack pointers, a CPU cycle is completed, upon which this function should
+ * be called to tick the other components attached to the CPU's bus.
+ * 
+ * CPU cycles due to bus access only occur when the CPU internally accesses the bus.
+ * 
+ * @param   p_CPU     The TM CPU instance to cycle.
+ * @param   p_Cycles  The number of cycles to cycle the CPU.
+ */
+void TM_CycleCPU (TM_CPU* p_CPU, uint32_t p_Cycles);
+
+/**
+ * @brief   Steps the CPU, either executing the next instruction or waiting for an interrupt to be
+ *          requested if the CPU is halted.
+ * 
+ * @param   p_CPU     The TM CPU instance to step.
+ */
+void TM_StepCPU (TM_CPU* p_CPU);
+
+/**
+ * @brief   Reads a byte from the bus at the specified address.
+ * @param   p_CPU     The TM CPU instance to read from.
+ * @param   p_Address The address to read from.
+ * @return  The byte read from the bus.
+ */
+uint8_t TM_ReadByte (TM_CPU* p_CPU, uint32_t p_Address);
+
+/**
+ * @brief   Reads a word from the bus at the specified address.
+ * @param   p_CPU     The TM CPU instance to read from.
+ * @param   p_Address The address to read from.
+ * @return  The word read from the bus.
+ */
+uint16_t TM_ReadWord (TM_CPU* p_CPU, uint32_t p_Address);
+
+/**
+ * @brief   Reads a double word from the bus at the specified address.
+ * @param   p_CPU     The TM CPU instance to read from.
+ * @param   p_Address The address to read from.
+ * @return  The double word read from the bus.
+ */
+uint32_t TM_ReadDoubleWord (TM_CPU* p_CPU, uint32_t p_Address);
+
+/**
+ * @brief   Writes a byte to the bus at the specified address.
+ * @param   p_CPU     The TM CPU instance to write to.
+ * @param   p_Address The address to write to.
+ * @param   p_Data    The byte to write to the bus.
+ */
+void TM_WriteByte (TM_CPU* p_CPU, uint32_t p_Address, uint8_t p_Data);
+
+/**
+ * @brief   Writes a word to the bus at the specified address.
+ * @param   p_CPU     The TM CPU instance to write to.
+ * @param   p_Address The address to write to.
+ * @param   p_Data    The word to write to the bus.
+ */
+void TM_WriteWord (TM_CPU* p_CPU, uint32_t p_Address, uint16_t p_Data);
+
+/**
+ * @brief   Writes a double word to the bus at the specified address.
+ * @param   p_CPU     The TM CPU instance to write to.
+ * @param   p_Address The address to write to.
+ * @param   p_Data    The double word to write to the bus.
+ */
+void TM_WriteDoubleWord (TM_CPU* p_CPU, uint32_t p_Address, uint32_t p_Data);
+
+/**
+ * @brief   Requests an interrupt to be handled by the CPU.
+ * 
+ * This function sets one of 16 bits in the CPU's interrupt flag register, corresponding to an
+ * interrupt to be handled. The CPU will then handle the interrupt on the next step, unless that
+ * bit in the interrupt enable register or the interrupt master enable flag is not set, in which
+ * case the interrupt will wait until both are set in order to be handled.
+ * 
+ * @param   p_CPU       The TM CPU instance to request the interrupt on.
+ * @param   p_Interrupt The interrupt to request, which the number of the bit in the interrupt flag
+ *                      register to set. The interrupt number must be between 0 and 15, inclusive.
+ */
+void TM_RequestInterrupt (TM_CPU* p_CPU, uint8_t p_Interrupt);
+
+/**
+ * @brief   Gets the value of a register in the TM CPU instance.
+ * @param   p_CPU       The TM CPU instance to get the register from.
+ * @param   p_Register  The register to get the value from.
+ * @return  The value of the register.
+ */
+uint32_t TM_GetRegister (const TM_CPU* p_CPU, TM_CPURegister p_Register);
+
+/**
+ * @brief   Sets the value of a register in the TM CPU instance.
+ * @param   p_CPU       The TM CPU instance to set the register in.
+ * @param   p_Register  The register to set the value of.
+ * @param   p_Value     The value to set the register to.
+ */
+void TM_SetRegister (TM_CPU* p_CPU, TM_CPURegister p_Register, uint32_t p_Value);
+
+/**
+ * @brief   Gets the value of a flag in the TM CPU instance.
+ * @param   p_CPU     The TM CPU instance to get the flag from.
+ * @param   p_Flag    The flag to get the value of.
+ * @return  The value of the flag.
+ */
+bool TM_GetFlag (const TM_CPU* p_CPU, TM_CPUFlag p_Flag);
+
+/**
+ * @brief   Sets the value of a flag in the TM CPU instance.
+ * @param   p_CPU     The TM CPU instance to set the flag in.
+ * @param   p_Flag    The flag to set the value of.
+ * @param   p_Value   The value to set the flag to.
+ */
+void TM_SetFlag (TM_CPU* p_CPU, TM_CPUFlag p_Flag, bool p_Value);
+
+/**
+ * @brief   Sets the values of the CPU's flags.
+ * 
+ * This function sets the values of the CPU's flags in a single call, allowing for more efficient
+ * setting of multiple flags at once. Passing `0` for a flag clears it; passing a positive value
+ * sets it; passing a negative value leaves it unchanged.
+ * 
+ * @param   p_CPU     The TM CPU instance to set the flags in.
+ * @param   p_Z       The value to set the zero flag to. `0` clears it, positive value sets it,
+ *                    negative value leaves it unchanged.
+ * @param   p_N       The value to set the subtract flag to. `0` clears it, positive value sets it,
+ *                    negative value leaves it unchanged.
+ * @param   p_H       The value to set the half-carry flag to. `0` clears it, positive value sets it,
+ *                    negative value leaves it unchanged.
+ * @param   p_C       The value to set the carry flag to. `0` clears it, positive value sets it,
+ *                    negative value leaves it unchanged.
+ */
+void TM_SetFlags (TM_CPU* p_CPU, int8_t p_Z, int8_t p_N, int8_t p_H, int8_t p_C);
+
+/**
+ * @brief   Gets the value of the CPU's interrupt enable register
+ * 
+ * The interrupt enable register is a special 16-bit register that contains a mask for the enabled
+ * interrupts. Each bit in the register corresponds to an interrupt, and if the bit is set, the
+ * interrupt is enabled; otherwise, it is disabled. The interrupt enable register is used to
+ * determine which interrupts are enabled and can be handled by the CPU.
+ * 
+ * @param   p_CPU     The TM CPU instance to get the interrupt master enable flag from.
+ * @return  `true` if interrupts are enabled; `false` otherwise.
+ */
+uint8_t TM_GetInterruptEnable (const TM_CPU* p_CPU);
+
+/**
+ * @brief   Gets the value of the CPU's interrupt flag register.
+ * 
+ * The interrupt flag register is a special 16-bit register that contains a mask for the pending
+ * interrupts. Each bit in the register corresponds to an interrupt, and if the bit is set, the
+ * interrupt is pending; otherwise, it is not.
+ * 
+ * @param   p_CPU     The TM CPU instance to get the interrupt flag register from.
+ * @return  The value of the interrupt flag register.
+ */
+uint8_t TM_GetInterruptFlags (const TM_CPU* p_CPU);
+
+/**
+ * @brief   Sets the value of the CPU's interrupt enable register.
+ * 
+ * The interrupt enable register is a special 16-bit register that contains a mask for the enabled
+ * interrupts. Each bit in the register corresponds to an interrupt, and if the bit is set, the
+ * interrupt is enabled; otherwise, it is disabled.
+ * 
+ * @param   p_CPU     The TM CPU instance to set the interrupt enable register in.
+ * @param   p_Value   The value to set the interrupt enable register to.
+ */
+void TM_SetInterruptEnable (TM_CPU* p_CPU, uint8_t p_Value);
+
+/**
+ * @brief   Sets the value of the CPU's interrupt flag register.
+ * 
+ * The interrupt flag register is a special 16-bit register that contains a mask for the pending
+ * interrupts. Each bit in the register corresponds to an interrupt, and if the bit is set, the
+ * interrupt is pending; otherwise, it is not.
+ * 
+ * @param   p_CPU     The TM CPU instance to set the interrupt flag register in.
+ * @param   p_Value   The value to set the interrupt flag register to.
+ */
+void TM_SetInterruptFlags (TM_CPU* p_CPU, uint8_t p_Value);
+
+/**
+ * @brief   Gets the value of the CPU's interrupt master enable flag.
+ * 
+ * The interrupt master enable flag is a special flag that indicates whether interrupts are enabled
+ * or disabled. If the flag is set, interrupts are enabled; otherwise, they are disabled. The
+ * interrupt master enable flag is used to determine whether the CPU should process interrupts or
+ * ignore them.
+ * 
+ * @param   p_CPU     The TM CPU instance to get the interrupt master enable flag from.
+ * @return  `true` if interrupts are enabled; `false` otherwise.
+ */
+bool TM_GetInterruptMasterEnable (const TM_CPU* p_CPU);
+
+/**
+ * @brief   Gets the value of the CPU's program counter.
+ * @param   p_CPU     The TM CPU instance to get the program counter from.
+ * @return  The value of the program counter.
+ */
+uint32_t TM_GetProgramCounter (const TM_CPU* p_CPU);
+
+/**
+ * @brief   Sets the value of the CPU's program counter.
+ * @param   p_CPU     The TM CPU instance to set the program counter in.
+ * @param   p_Value   The value to set the program counter to.
+ */
+void TM_SetProgramCounter (TM_CPU* p_CPU, uint32_t p_Value);
+
+/**
+ * @brief   Gets the value of the CPU's data stack pointer.
+ * @param   p_CPU     The TM CPU instance to get the stack pointer from.
+ * @return  The value of the data stack pointer.
+ */
+uint32_t TM_GetDataStackPointer (const TM_CPU* p_CPU);
+
+/**
+ * @brief   Gets the value of the CPU's call stack pointer.
+ * @param   p_CPU     The TM CPU instance to get the stack pointer from.
+ * @return  The value of the call stack pointer.
+ */
+uint32_t TM_GetCallStackPointer (const TM_CPU* p_CPU);
+
+/**
+ * @brief   Gets the value of the CPU's error code register.
+ * 
+ * The error code register is a special 8-bit register that contains the last error code generated
+ * by the CPU, or set with the `SEC` instruction. It is used to indicate whether or not something
+ * went wrong during the execution of the CPU's last instruction or ticking of its bus's components.
+ * 
+ * @param   p_CPU     The TM CPU instance to get the error code from.
+ * @return  The value of the error code register.
+ */
+uint8_t TM_GetErrorCode (const TM_CPU* p_CPU);
+
+/**
+ * @brief   Sets the value of the CPU's error code register.
+ * 
+ * The error code register is a special 8-bit register that contains the last error code generated
+ * by the CPU, or set with the `SEC` instruction. It is used to indicate whether or not something
+ * went wrong during the execution of the CPU's last instruction or ticking of its bus's components.
+ * Setting the error code this way also sets the CPU's stop flag.
+ * 
+ * @param   p_CPU           The TM CPU instance to set the error code in.
+ * @param   p_ErrorCode     The value to set the error code register to.
+ * @return  `true` if the error code set is zero, indicating a normal exit; `false` otherwise.
+ */
+bool TM_SetErrorCode (TM_CPU* p_CPU, uint8_t p_ErrorCode);
+
+/**
+ * @brief   Gets the value of the CPU's halt flag.
+ * 
+ * The halt flag is a special flag that indicates whether or not the CPU is currently halted. The
+ * CPU is halted by the `HALT` instruction, and it remains halted until an interrupt is requested,
+ * or the CPU is reset.
+ * 
+ * @param   p_CPU     The TM CPU instance to get the halt flag from.
+ * @return  `true` if the CPU is halted; `false` otherwise.
+ */
+bool TM_IsHalted (const TM_CPU* p_CPU);
+
+/**
+ * @brief   Gets the value of the CPU's stop flag.
+ * 
+ * The stop flag is a special flag that indicates whether or not the CPU is currently stopped. The
+ * CPU is stopped by the `STOP` instruction, or when the error code register is set to a non-zero
+ * value with the `TM_SetErrorCode` function. The CPU remains stopped until it is reset.
+ * 
+ * @param   p_CPU     The TM CPU instance to get the stop flag from.
+ * @return  `true` if the CPU is stopped; `false` otherwise.
+ */
+bool TM_IsStopped (const TM_CPU* p_CPU);
---
+++ projects/tm/include/TM/Common.h
@@ -0,0 +1,244 @@
+/**
+ * @file  TM/Common.h
+ * @brief Contains commonly-used includes, constants, typedefs and macros.
+ */
+
+#pragma once
+
+// Include Files ///////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <time.h>
+#include <assert.h>
+#include <errno.h>
+
+// Memory Map Constants ////////////////////////////////////////////////////////////////////////////
+
+#define TM_ROM_BEGIN            0x00000000
+#define TM_MDATA_BEGIN          0x00000000
+#define TM_MDATA_END            0x00000FFF
+#define TM_RST_BEGIN            0x00001000
+#define TM_RST_END              0x00001FFF
+#define TM_INT_BEGIN            0x00002000
+#define TM_INT_END              0x00002FFF
+#define TM_CODE_BEGIN           0x00003000
+#define TM_CODE_END             0x3FFFFFFF
+#define TM_DATA_BEGIN           0x40000000
+#define TM_DATA_END             0x7FFFFFFF
+#define TM_ROM_END              0x7FFFFFFF
+
+#define TM_RAM_BEGIN            0x80000000
+#define TM_DRAM_BEGIN           0x80000000
+#define TM_DRAM_END             0xDFFFFFFF
+#define TM_XRAM_BEGIN           0xE0000000
+#define TM_XRAM_END             0xFFFCFFFF
+#define TM_DSTACK_BEGIN         0xFFFD0000
+#define TM_DSTACK_END           0xFFFDFFFF
+#define TM_CSTACK_BEGIN         0xFFFE0000
+#define TM_CSTACK_END           0xFFFEFFFF
+#define TM_QRAM_BEGIN           0xFFFF0000
+#define TM_QRAM_END             0xFFFFFEFF
+#define TM_IO_BEGIN             0xFFFFFF00
+#define TM_IO_END               0xFFFFFFFF
+#define TM_RAM_END              0xFFFFFFFF
+
+// CPU Register Enumeration ////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief   Enumerates the CPU's general-purpose registers.
+ * 
+ * The TM CPU has four 32-bit general-purpose registers: `A`, `B`, `C` and `D`. The registers can
+ * be accessed as follows:
+ * 
+ * - As a whole register (e.g. `A`).
+ * 
+ * - As a 16-bit register (e.g. `AW`, the lower 16 bits of `A`).
+ * 
+ * - As an 8-bit register (e.g. `AH` and `AL`, the upper and lower 8 bits of `AW`, respectively).
+ * 
+ * The size of the registers can be determined by checking the lower 2 bits of the register's
+ * enumeration value:
+ * 
+ * - `00`: 32-bit register
+ * 
+ * - `01`: 16-bit register
+ * 
+ * - `10` and `11`: 8-bit registers
+ * 
+ * The register being accessed, in the meantime, can be determined by the upper 2 bits:
+ * 
+ * - `00`: Register `A`, a general-purpose register, designated as the accumulator.
+ * 
+ * - `01`: Register `B`, a general-purpose register, traditionally used as a base register.
+ * 
+ * - `10`: Register `C`, a general-purpose register, traditionally used as a counter register.
+ * 
+ * - `11`: Register `D`, a general-purpose register, traditionally used as a data register.
+ * 
+ */
+typedef enum TM_CPURegister
+{
+    TM_REG_A  = 0b0000,
+    TM_REG_AW = 0b0001,
+    TM_REG_AH = 0b0010,
+    TM_REG_AL = 0b0011,
+
+    TM_REG_B  = 0b0100,
+    TM_REG_BW = 0b0101,
+    TM_REG_BH = 0b0110,
+    TM_REG_BL = 0b0111,
+
+    TM_REG_C  = 0b1000,
+    TM_REG_CW = 0b1001,
+    TM_REG_CH = 0b1010,
+    TM_REG_CL = 0b1011,
+
+    TM_REG_D  = 0b1100,
+    TM_REG_DW = 0b1101,
+    TM_REG_DH = 0b1110,
+    TM_REG_DL = 0b1111,
+} TM_CPURegister;
+
+// CPU Flag Enumeration ////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief   Enumerates the CPU's flags.
+ * 
+ * The TM CPU keeps a special 8-bit register called the flags register, which contains the status of
+ * the CPU's last operation. The register is modeled after the flags register of the Sharp LR35902 CPU,
+ * whose flags are laid out as follows:
+ * 
+ * - `Z`: The zero flag, used to indicate if the result of the last operation was zero.
+ * 
+ * - `N`: The subtract flag, used to indicate if the last operation was (or involved) a subtraction.
+ * 
+ * - `H`: The half-carry flag, used to indicate if a carry occurred in a portion of the last operation's
+ *        result.
+ * 
+ * - `C`: The carry flag, used to indicate if a carry occurred in the last operation's result.
+ */
+typedef enum TM_CPUFlag
+{
+    TM_FLAG_Z = 7, ///< @brief The zero flag, used to indicate if the result of the last operation was zero.
+    TM_FLAG_N = 6, ///< @brief The subtract flag, used to indicate if the last operation was (or involved) a subtraction.
+    TM_FLAG_H = 5, ///< @brief The half-carry flag, used to indicate if a carry occurred in a portion of the last operation's result.
+    TM_FLAG_C = 4, ///< @brief The carry flag, used to indicate if a carry occurred in the last operation's result.
+} TM_CPUFlag;
+
+// CPU Execution Condition Enumeration /////////////////////////////////////////////////////////////
+
+/**
+ * @brief   Enumerates the CPU's execution conditions.
+ * 
+ * Certain instructions in the TM CPU's instruction set - particularly the control-transfer
+ * instructions - can be executed conditionally, depending on the state of the CPU's flags.
+ */
+typedef enum TM_CPUCondition
+{
+    TM_COND_NONE = 0, ///< @brief No condition, used to indicate that the CPU should execute the next instruction.
+    TM_COND_Z    = 1, ///< @brief The zero condition, used to indicate that the CPU should execute the next instruction if the zero flag is set.
+    TM_COND_NZ   = 2, ///< @brief The not zero condition, used to indicate that the CPU should execute the next instruction if the zero flag is not set.
+    TM_COND_C    = 3, ///< @brief The carry condition, used to indicate that the CPU should execute the next instruction if the carry flag is set.
+    TM_COND_NC   = 4, ///< @brief The not carry condition, used to indicate that the CPU should execute the next instruction if the carry flag is not set.
+} TM_CPUCondition;
+
+// CPU Instruction Enumeration /////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief   Enumerates the instructions in the TM CPU's instruction set.
+ * 
+ * Each instruction in the TM CPU's instruction set is represented by a unique 16-bit value, whose
+ * nibbles are laid out in the following way: `0xIIXY`...
+ * 
+ * - `II`: The instruction's opcode, which determines the instruction's type and operation.
+ * 
+ * - `X`: The instruction's first parameter, which can be a register or an execution condition.
+ * 
+ * - `Y`: The instruction's second parameter, which is usually a register.
+ * 
+ * The instructions' enumeration values are their base opcodes, which don't account for variations
+ * in the instruction.
+ */
+typedef enum TM_CPUInstruction
+{
+    TM_INST_NOP  = 0x0000, ///< @brief No operation, used to indicate that the CPU should do nothing.
+    TM_INST_STOP = 0x0100, ///< @brief Stop the CPU, used to indicate that the CPU should stop executing instructions.
+    TM_INST_HALT = 0x0200, ///< @brief Halt the CPU, used to indicate that the CPU should halt execution until an interrupt occurs.
+    TM_INST_SEC  = 0x0300, ///< @brief Set the error code register. The value to set is in the instruction's lower 8 bits.
+    TM_INST_CEC  = 0x0400, ///< @brief Clear the error code register, setting it to zero.
+    TM_INST_DI   = 0x0500, ///< @brief Disable interrupts, used to indicate that the CPU should ignore interrupts.
+    TM_INST_EI   = 0x0600, ///< @brief Enable interrupts, used to indicate that the CPU should process interrupts, starting at the next step.
+    TM_INST_DAA  = 0x0700, ///< @brief Decimal adjust accumulator, used to adjust the value in one of the accumulator registers.
+    TM_INST_SCF  = 0x0800, ///< @brief Set the carry flag, used to set the CPU's carry flag.
+    TM_INST_CCF  = 0x0900, ///< @brief Complement the carry flag, used to toggle the CPU's carry flag.
+    TM_INST_LD   = 0x1000, ///< @brief Load a value into a register.
+    TM_INST_LDQ  = 0x1300, ///< @brief Load a value from QRAM into a register.
+    TM_INST_LDH  = 0x1500, ///< @brief Load a value from a hardware I/O port into a register.
+    TM_INST_ST   = 0x1700, ///< @brief Store a value from a register into memory.
+    TM_INST_STQ  = 0x1900, ///< @brief Store a value from a register into QRAM.
+    TM_INST_STH  = 0x1B00, ///< @brief Store a value from a register into a hardware I/O port.
+    TM_INST_MV   = 0x1D00, ///< @brief Move a value from one register to another.
+    TM_INST_PUSH = 0x1E00, ///< @brief Push a value onto the stack.
+    TM_INST_POP  = 0x1F00, ///< @brief Pop a value from the stack into a register.
+    TM_INST_JMP  = 0x2000, ///< @brief Jump to a specified address.
+    TM_INST_JPB  = 0x2300, ///< @brief Jump by a specified offset.
+    TM_INST_CALL = 0x2400, ///< @brief Call a subroutine at a specified address.
+    TM_INST_RST  = 0x2500, ///< @brief Call one of 16 restart vector handlers.
+    TM_INST_RET  = 0x2600, ///< @brief Return from a subroutine.
+    TM_INST_RETI = 0x2700, ///< @brief Return from an interrupt.
+    TM_INST_JPS  = 0x2800, ///< @brief Jump to the start of the program.
+    TM_INST_INC  = 0x3000, ///< @brief Increment a value.
+    TM_INST_DEC  = 0x3200, ///< @brief Decrement a value.
+    TM_INST_ADD  = 0x3400, ///< @brief Add two values.
+    TM_INST_ADC  = 0x3700, ///< @brief Add two values with carry.
+    TM_INST_SUB  = 0x3A00, ///< @brief Subtract two values.
+    TM_INST_SBC  = 0x3D00, ///< @brief Subtract two values with borrow.
+    TM_INST_AND  = 0x4000, ///< @brief Bitwise AND two values.
+    TM_INST_OR   = 0x4300, ///< @brief Bitwise OR two values.
+    TM_INST_XOR  = 0x4600, ///< @brief Bitwise XOR two values.
+    TM_INST_NOT  = 0x4900, ///< @brief Bitwise NOT a value.
+    TM_INST_CMP  = 0x5000, ///< @brief Compare two values.
+    TM_INST_SLA  = 0x6000, ///< @brief Shift a value left arithmetic.
+    TM_INST_SRA  = 0x6200, ///< @brief Shift a value right arithmetic.
+    TM_INST_SRL  = 0x6400, ///< @brief Shift a value right logical.
+    TM_INST_RL   = 0x6600, ///< @brief Rotate a value left through the carry.
+    TM_INST_RLC  = 0x6800, ///< @brief Rotate a value left.
+    TM_INST_RR   = 0x6A00, ///< @brief Rotate a value right through the carry.
+    TM_INST_RRC  = 0x6C00, ///< @brief Rotate a value right.
+    TM_INST_BIT  = 0x7000, ///< @brief Bit test.
+    TM_INST_RES  = 0x7200, ///< @brief Bit reset.
+    TM_INST_SET  = 0x7400, ///< @brief Bit set.
+    TM_INST_SWAP = 0x7600, ///< @brief Swap the nibbles/bytes/words of a value.
+} TM_CPUInstruction;
+
+// CPU Error Code Enumeration //////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief   Enumerates the error codes generated by the CPU.
+ * 
+ * The TM CPU generates error codes when it encounters an error during execution. The error codes
+ * are used to indicate the type of error that occurred, and can be set with the `SEC` instruction.
+ */
+typedef enum TM_ErrorCode
+{
+    TM_EC_OK,                           ///< @brief No error, used to indicate that the CPU's last operation was successful.
+    TM_EC_INVALID_OPCODE,               ///< @brief Invalid opcode, used to indicate that the CPU encountered an invalid opcode.
+    TM_EC_INVALID_ARGUMENT,             ///< @brief Invalid argument, used to indicate that the CPU encountered an invalid argument.
+    TM_EC_BUS_READ,                     ///< @brief Bus read error, used to indicate that the CPU encountered an error while reading from the bus.
+    TM_EC_BUS_WRITE,                    ///< @brief Bus write error, used to indicate that the CPU encountered an error while writing to the bus.
+    TM_EC_BAD_READ,                     ///< @brief Bad read error, used to indicate that the CPU attempted to read from non-readable memory.
+    TM_EC_BAD_WRITE,                    ///< @brief Bad write error, used to indicate that the CPU attempted to write to non-writable memory.
+    TM_EC_BAD_EXECUTE,                  ///< @brief Bad execute error, used to indicate that the CPU attempted to execute non-executable memory.
+    TM_EC_DATA_STACK_OVERFLOW,          ///< @brief Data stack overflow error, used to indicate that the CPU attempted to push a value onto the data stack when it was full.
+    TM_EC_DATA_STACK_UNDERFLOW,         ///< @brief Data stack underflow error, used to indicate that the CPU attempted to pop a value from the data stack when it was empty.
+    TM_EC_CALL_STACK_OVERFLOW,          ///< @brief Call stack overflow error, used to indicate that the CPU attempted to push an address onto the call stack when it was full.
+    TM_EC_HARDWARE_FAULT,               ///< @brief Hardware fault error, used to indicate that the CPU encountered an error while ticking one of the bus's attached components.
+} TM_ErrorCode;
